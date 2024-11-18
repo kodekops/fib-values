37f23a93728392e461110b9b4c4b05d083397d81
Khadga Bahadur Shrestha <khadgalovecoding2016@gmail.com>
2024-11-18 13:17:00 +0545

    added config for skaffold for hot reload

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
       <nav className="navbar navbar-expand bg-body-tertiary bg-dark">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Fib Finder(v2)
+            Fib Finder
           </Link>
           <button
             className="navbar-toggler"
